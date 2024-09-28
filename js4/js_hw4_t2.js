fa0430e18995dae306b709d9f4bde8810ba52738
angelok199751 <125383342+angelok199751@users.noreply.github.com>
2024-09-28 23:29:11 +0300

    modificate file js_hw4_t2.js

--- js4/js_hw4_t2.js
+++ js4/js_hw4_t2.js
@@ -1,13 +1,18 @@
-const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
-const myBakesPizzas = ['4 season', 'Peperoni', 'Caprichosa', 'Vegetarian', 'Cheff Pizza'];
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'Hawai'];
+const myBakesPizzas = ['4 season', 'Peperoni', 'caprichosa', 'Vegetarian', 'Cheff Pizza', 'hawai'];
+
+const lowerCasecompetitorPizzas = competitorPizzas.map(competitorPizzas => competitorPizzas.toLowerCase());
+const lowerCasemyBakesPizzas = myBakesPizzas.map(myBakesPizzas => myBakesPizzas.toLowerCase());
 
 const diffPizzas = [];
 
-for (let i = 0; i < myBakesPizzas.length; i++) {
-    
-    if (!competitorPizzas.includes(myBakesPizzas[i])) {
-        diffPizzas.push(myBakesPizzas[i].toLowerCase());
+for (let i = 0; i < lowerCasemyBakesPizzas.length; i++) {
+
+    if (!lowerCasecompetitorPizzas.includes(lowerCasemyBakesPizzas[i])) {
+        diffPizzas.push(lowerCasemyBakesPizzas[i]);
     }
- 
+
 }
 console.log(diffPizzas.length ? diffPizzas : null);
+ 
+
