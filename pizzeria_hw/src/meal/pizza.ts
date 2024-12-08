5299329a28e2482971037b20082f81a2f2fa2f28
angelok199751 <125383342+angelok199751@users.noreply.github.com>
2024-12-09 00:47:54 +0300

    Update pizza.ts

--- pizzeria_hw/src/meal/pizza.ts
+++ pizzeria_hw/src/meal/pizza.ts
@@ -7,7 +7,7 @@ export class Pizza extends Meal {
   constructor(
     public name: PIZZA_NAMES,
     public size: PIZZA_SIZE,
-    public price: number = pizzaReceipts[name].prices[size],
+    protected price: number = pizzaReceipts[name].prices[size],
     public toppings: string[] = pizzaReceipts[name].toppings,
     public dough: DOUGH_TYPE,
     public extraToppings: (keyof typeof TOPPINGS)[] = []
